385c62c6dde94a48d729a2b19acaf569d3b646b5
wangjinzhu <wangjinzhu@ifunq.com>
2018-08-27 11:22:00 +0800

    reverse

--- leetcode.py
+++ leetcode.py
@@ -3,26 +3,28 @@
 # author = yexiaozhu
 
 class Solution:
-    def convert(self,s, numRows):
+    def reverse(self, x):
         """
-        :param s:  str
-        :param numRows: int
-        :return:  str
+        :param x: int
+        :return:  int
         """
-        if numRows == 1:
-            return s
-        zigzag = ['' for i in range(numRows)] # 初始化zigzag为['','','']
-        row = 0
-        step = 1
-        for c in s:
-            if row == 0:
-                step = 1
-            if row == numRows - 1:
-                step = -1
-            zigzag[row] += c
-            # print(zigzag)
-            row  +=  step
-        return ''.join(zigzag)
+        result = 0
+        flag = 1
+        if x < 0:
+            x = -x
+            flag = 0
+        while(x):
+            a = x%10
+            result = result*10 + a
+            # print(result)
+            x = x//10
+        if result>2**31-1 or result<-2**31:
+            return 0
+        else:
+            if flag==0:
+                return -result
+            else:
+                return result
         
 if __name__ == '__main__':
-    Solution().convert("PAYPALISHIRING", 3)
+    Solution().reverse(123)
