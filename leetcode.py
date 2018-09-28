4a0fc96af6ab5bdba07236e3ea32201214412f9c
wangjinzhu <wangjinzhu@ifunq.com>
2018-09-28 11:45:12 +0800

    3Sum

--- leetcode.py
+++ leetcode.py
@@ -4,24 +4,43 @@
 
 import re
 class Solution:
-    def longestCommonPrefix(self, strs):
+    def threeSum(self, nums):
         """
-        :param strs: List[str]
-        :return: str
+        :param nums:  List[int]
+        :return:  List[List[int]]
         """
-        res = ""
-        if len(strs) == 0:
-            return ""
-        print(zip(*strs))
-        for each in zip(*strs):
-            print(each)
-            print(set(each))
-            if len(set(each)) == 1:
-                res += each[0]
-            else:
-                return res
-        return res
+        nums.sort()
+        res = []
+        lennums = len(nums)
+
+        for i in range(lennums):
+            left = i + 1
+            right = lennums - 1
+    
+            if i > 0 and nums[i - 1] == nums[i]:
+                left += 1
+                continue
+    
+            while left < right:
+                sumthree = nums[i] + nums[left] + nums[right]
+                if sumthree == 0:
+                    res_col = [nums[i], nums[left], nums[right]]
+                    res.append(res_col)
+                    left += 1
+                    right -= 1
+            
+                    while nums[left] == nums[left - 1] and left < right:
+                        left += 1
+                    while nums[right] == nums[right + 1] and left < right:
+                        right -= 1
         
+                if sumthree < 0:
+                    left += 1
+                if sumthree > 0:
+                    right -= 1
+        return res
     
 if __name__ == '__main__':
-    print(Solution().longestCommonPrefix(["flower","flow","flight"]))
+    print(Solution().threeSum([-1, 0, 1, 2, -1, -4]))
+    print(Solution().threeSum([0,0,0,0]))
+    print(Solution().threeSum([-2,0,1,1,2]))
