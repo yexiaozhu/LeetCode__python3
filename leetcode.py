83af159934765f1e02ef3a4955f9d624388cb816
wangjinzhu <wangjinzhu@ifunq.com>
2018-10-10 16:15:46 +0800

    generate parentheses

--- leetcode.py
+++ leetcode.py
@@ -2,45 +2,30 @@
 # coding = utf-8
 # author = yexiaozhu
 
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
 class Solution:
-    def mergeTwoLists(self, l1, l2):
+    def generateParentheses(self, n):
         """
-        :param l1: ListNode
-        :param l2: ListNode
-        :return: ListNode
+        :param n: int
+        :return: List[str]
         """
-        if l1 == None: return l2
-        if l2 == None: return l1
-        
-        head = None
-        if l1.val < l2.val:
-            head = l1
-            l1 = l1.next
-        else:
-            head = l2
-            l2 = l2.next
-        mylist = head
-        while l1 != None and l2 != None:
-            if l1.val < l2.val:
-                mylist.next = l1
-                l1 = l1.next
-            else:
-                mylist.next = l2
-                l2 = l2.next
-            mylist = mylist.next
-        if l1 == None:
-            mylist.next = l2
-        else:
-            mylist.next = l1
-        return head
-        
+        res = []
+        self.generate(n, n, "", res)
+        return res
+    
+    def generate(self, left, right, str, res):
+        if left == 0 and right == 0:
+            res.append(str)
+            print(res)
+            return
+        if left > 0:
+            print('left:', left, str)
+            self.generate(left-1, right, str+'(', res)
+        if right > left:
+            print('right', right, str)
+            self.generate(left, right-1, str+')', res)
+            
+if __name__ == '__main__':
+    print(Solution().generateParentheses(3))
+
         
 
-if __name__ == '__main__':
-    print(Solution().isValid('()[]{}'))
-    print(Solution().isValid(''))
